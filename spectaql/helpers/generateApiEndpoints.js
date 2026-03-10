4c796d472ef07c40b57d0a9828f0dc20f08bbaae
Edijs Dunavskis <dunavskisedijs@gmail.com>
2026-03-10 10:49:39 +0200

    fix: reenable url servers config

--- spectaql/helpers/generateApiEndpoints.js
+++ spectaql/helpers/generateApiEndpoints.js
@@ -1,13 +1,39 @@
-module.exports = function (options) {
-    const { servers } = options?.data?.root || {}
+module.exports = function (options) {var _options$data, _options$data2;
+  const { url, server, servers } = (options === null || options === void 0 || (_options$data = options.data) === null || _options$data === void 0 ? void 0 : _options$data.root) || {};
 
-    return servers
-        .map(
-            (server) =>
-                server.description.trim() +
-                ':\n' +
-                server.url.trim() +
-                (server === servers[servers.length - 1] ? '' : '\n​\n')
-        )
-        .join('')
-}
+  const displayAllServers =
+  (options === null || options === void 0 || (_options$data2 = options.data) === null || _options$data2 === void 0 || (_options$data2 = _options$data2.root) === null || _options$data2 === void 0 || (_options$data2 = _options$data2.allOptions) === null || _options$data2 === void 0 || (_options$data2 = _options$data2.specData) === null || _options$data2 === void 0 || (_options$data2 = _options$data2.spectaql) === null || _options$data2 === void 0 ? void 0 : _options$data2.displayAllServers) ===
+  true;
+  if (!displayAllServers) {
+    if (url) {
+      return url;
+    }
+    if (server !== null && server !== void 0 && server.url) {
+      return server.url;
+    }
+    const fallbackServer =
+    (servers === null || servers === void 0 ? void 0 : servers.find((server) => server.url && server.production)) || (
+    servers === null || servers === void 0 ? void 0 : servers.find((server) => server.url));
+    if (fallbackServer) {
+      return fallbackServer.url;
+    }
+  }
+
+  if (!(servers !== null && servers !== void 0 && servers.length)) {
+    return '<<url is missing>>';
+  }
+
+  return servers.
+  map(
+    (server) => {
+      var _server$description, _server$url;
+      const separator = server === servers[servers.length - 1] ? '' : '\n​\n'; // Adds an extra line between servers
+      return (
+        '# ' + (
+        ((_server$description = server.description) === null || _server$description === void 0 ? void 0 : _server$description.trim()) || 'Endpoint') +
+        ':\n' + (
+        ((_server$url = server.url) === null || _server$url === void 0 ? void 0 : _server$url.trim()) || '<<url is missing>>') + separator
+      );}
+  ).
+  join('');
+};
